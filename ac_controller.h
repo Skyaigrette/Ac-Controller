ad5b2d0d0a01752f63942f0fbb7be2b03bf1c538
Göktuğ Furkan Arıca <67566490+Skyaigrette@users.noreply.github.com>
2024-07-31 01:03:53 +0300

    Update ac_controller.h

--- ac_controller.h
+++ ac_controller.h
@@ -10,16 +10,15 @@ const uint16_t irPin = 5;
 int freq = 38;
 bool acStatus = false;
 IRMitsubishi112 ac(irPin);
-//String SwingV[] = {"Lowest", "Low", "Middle", "High", "Highest", "Auto"};
 String SwingV[] = {"Highest", "High", "Middle", "Low", "Lowest", "Auto"};
 
-void initializeIR(){
+void initializeIR() {
   Serial.print("Swing = "); Serial.println(kMitsubishi112SwingVLowest); Serial.println(kMitsubishi112SwingVLow); Serial.println(kMitsubishi112SwingVMiddle);
   Serial.println(kMitsubishi112SwingVHigh); Serial.println(kMitsubishi112SwingVHighest); Serial.println(kMitsubishi112SwingVAuto);
   ac.begin();
   delay(2000);
   ac.setTemp(20);
-  ac.setSwingV(kMitsubishi112SwingVLowest); // lowest - low - middle - high - highest - auto 
+  ac.setSwingV(kMitsubishi112SwingVLowest); // lowest - low - middle - high - highest - auto
   ac.setMode(kMitsubishi112Cool); // cool - heat - auto - dry
   ac.setFan(kMitsubishi112FanLow); // min - low - med - max
 }
@@ -37,70 +36,74 @@ void printState() {
   Serial.println();
 }
 
-//void sendIR(uint16_t irData[]){
-//  for(int i = 0; i < 5; i++ ) {
-//    irsend.sendRaw(irData, sizeof(powerOnData) / sizeof(powerOnData[0]) , freq);
-//    Serial.print("X");
-//    delay(150);
-//  }
-//  Serial.println(" || Success");
-//}
 
-void powerOn(){
+void powerOn(bool sendD = true) {
   Serial.println("Opening A/C");
-  //sendIR(powerOnData);
-  ac.on();
-  for(int i = 0; i<5;i++){
-    ac.send();
+  ac.setPower(true);
+  if (sendD) {
+    for (int i = 0; i < 5; i++) {
+      ac.send();
+    }
   }
+
   //ac.send();
   digitalWrite(acLed, HIGH);
-  acStatus = true;  
+  acStatus = true;
   delay(2000);
 
 }
 
-void powerOff(){
+void powerOff(bool sendD = true) {
   Serial.println("Closing A/C");
   //sendIR(powerOffData);
-  ac.off();
-  ac.send();
+  ac.setPower(false );
+  if (sendD) {
+    for (int i = 0; i < 5; i++) {
+      ac.send();
+    }
+  }
   digitalWrite(acLed, LOW);
   acStatus = false;
   delay(2000);
 }
 
-void setSwingVAC(int buttonPin){
+void setSwingVAC(int buttonPin) {
   int potV;
   int swingCode;
   int analogPower;
-  while(digitalRead(buttonPin) == LOW){
+  while (digitalRead(buttonPin) == LOW) {
     potV = analogRead(pot);
     swingCode = map(potV, 0, 4096, 1, 6);
     analogPower = 255 / swingCode; // 42 * 6 = 252 (apprx. 256)
     analogWrite(swingLed, analogPower),
-    Serial.print("Swing Code = "); Serial.println(swingCode);
-    Serial.print("Swing Mode = "); Serial.println(SwingV[ swingCode-1 ]);
+                Serial.print("Swing Code = "); Serial.println(swingCode);
+    Serial.print("Swing Mode = "); Serial.println(SwingV[ swingCode - 1 ]);
     delay(750);
-    if(digitalRead(buttonPin)){
+    if (digitalRead(buttonPin)) {
       analogWrite(swingLed, LOW);
       Serial.println("Release Button");
       delay(2000);
       break;
     }
   }
-  if(swingCode == 6){swingCode = 7;}
+  if (swingCode == 6) {
+    swingCode = 7;
+  }
   ac.setSwingV(swingCode);
   ac.send();
 }
 
-//irsend.sendRaw(powerOnData, sizeof(powerOnData) / sizeof(powerOnData[0]), freq);  // Send a raw data captured at 38kHz.
-
-  /*
-   * lowest = 5
-   * low = 4
-   * middle = 3
-   * high = 2
-   * highest = 1
-   * auto = 7
-   */
+bool handleAC(float temperature, long timeDifferenceAC, bool firstRun) {
+  if ( (temperature > 29 || temperature == 29) && acStatus == false && (timeDifferenceAC > 600000 || firstRun) ) { // 10 mins
+    firstRun = false;
+    powerOn();
+    return true;
+  }
+  else if (
+    ( (temperature < 27 && (timeDifferenceAC > 600000 || firstRun)) || (temperature < 24) ) && acStatus == true) {
+    firstRun = false;
+    powerOff();
+    return true;
+  }
+  return false;
+}
